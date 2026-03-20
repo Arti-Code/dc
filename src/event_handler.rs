f1d85bd84d97f8723441c1909a9c0e56ca76add5
Arturo <41492101+Arti-Code@users.noreply.github.com>
2026-03-20 13:33:26 +0100

    v0.7.2

--- src/event_handler.rs
+++ src/event_handler.rs
@@ -1,8 +1,8 @@
 use std::{sync::Arc, time::Duration};
 
-use bytes::BytesMut;
+//use bytes::BytesMut;
 use futures::FutureExt;
-use signaler::command::generate_description;
+//use signaler::command::generate_description;
 use webrtc::{
     data_channel::{DataChannel, DataChannelEvent}, 
     peer_connection::{PeerConnectionEventHandler, RTCIceGatheringState, RTCPeerConnectionState}, 
@@ -47,19 +47,36 @@ impl PeerConnectionEventHandler for CameraHandler {
     } */
 
     async fn on_connection_state_change(&self, state: RTCPeerConnectionState) {
-        let mut state_info = String::new();
-        if state == RTCPeerConnectionState::Failed {
-            state_info = format!("{}{}", "[PEER CONNECTION]: ".to_string().bold().red(), state.to_string().bold().red());
-            let _ = self.done_tx.try_send(());
-        } else if state == RTCPeerConnectionState::Disconnected {
-            state_info = format!("{}{}", "[PEER CONNECTION]: ".to_string().bold().yellow(), state.to_string().bold().yellow());
-            let _ = self.done_tx.try_send(());
-        } else if state == RTCPeerConnectionState::Connected {
-            state_info = format!("{}{}", "[PEER CONNECTION]: ".to_string().bold().green(), state.to_string().bold().green());
-            let _ = self.connected_tx.try_send(());
-        } else if state == RTCPeerConnectionState::Closed {
-            state_info = format!("{}{}", "[PEER CONNECTION]: ".to_string().bold().yellow(), state.to_string().bold().yellow());
-            let _ = self.done_tx.try_send(());
+        let state_info: String;
+        match state {
+            RTCPeerConnectionState::Failed => {
+                state_info = format!("{}{}", "[PEER CONNECTION]: ".to_string().bold().red(), state.to_string().bold().red());
+                let _ = self.done_tx.try_send(());
+            },
+            RTCPeerConnectionState::Disconnected => {
+                state_info = format!("{}{}", "[PEER CONNECTION]: ".to_string().bold().yellow(), state.to_string().bold().yellow());
+                let _ = self.done_tx.try_send(());
+            },
+            RTCPeerConnectionState::Connected => {
+                state_info = format!("{}{}", "[PEER CONNECTION]: ".to_string().bold().green(), state.to_string().bold().green());
+                let _ = self.connected_tx.try_send(());
+            },
+            RTCPeerConnectionState::Closed => {
+                state_info = format!("{}{}", "[PEER CONNECTION]: ".to_string().bold().yellow(), state.to_string().bold().yellow());
+                let _ = self.done_tx.try_send(());
+            },
+            RTCPeerConnectionState::New => {
+                state_info = format!("{}{}", "[PEER CONNECTION]: ".to_string().bold().bright_blue(), state.to_string().bold().bright_blue());
+                let _ = self.connected_tx.try_send(());
+            },
+            RTCPeerConnectionState::Connecting => {
+                state_info = format!("{}{}", "[PEER CONNECTION]: ".to_string().bold().bright_green(), state.to_string().bold().bright_green());
+                let _ = self.connected_tx.try_send(());
+            },
+            RTCPeerConnectionState::Unspecified => {
+                state_info = format!("{}{}", "[PEER CONNECTION]: ".to_string().bold().bright_yellow(), state.to_string().bold().bright_yellow());
+                let _ = self.connected_tx.try_send(());
+            },
         }
         println!("{}", &state_info);
     }
@@ -87,10 +104,10 @@ impl PeerConnectionEventHandler for CameraHandler {
                             }
                         }
                         _ = send_timer.as_mut().fuse() => {
-                            let message = generate_description(32);
-                            println!("<== '{message}'");
-                            let _ = dc.send(BytesMut::from(message.as_bytes())).await;
-                            send_timer = Box::pin(sleep(Duration::from_secs(5)));
+                            //let message = generate_description(32);
+                            //println!("<== '{message}'");
+                            //let _ = dc.send(BytesMut::from(message.as_bytes())).await;
+                            //send_timer = Box::pin(sleep(Duration::from_secs(5)));
                         }
                     }
                 } else {
@@ -98,31 +115,30 @@ impl PeerConnectionEventHandler for CameraHandler {
                         Some(DataChannelEvent::OnOpen) => {
                             println!("{}", "datachannel open".to_string().green().bold());
                             opened = true;
-                            send_timer = Box::pin(sleep(Duration::from_secs(5)));
+                            //send_timer = Box::pin(sleep(Duration::from_secs(5)));
                         }
                         Some(DataChannelEvent::OnClose) => {
-                            let _ = done_tx.try_send(());
-                            //println!("datachannel close");
                             println!("{}", "datachannel closed".to_string().red().bold());
+                            let _ = done_tx.try_send(());
                             break;
                         },
                         Some(DataChannelEvent::OnClosing) => {
-                            //let _ = done_tx.try_send(());
                             println!("{}", "datachannel closing".to_string().yellow().bold());
+                            let _ = done_tx.try_send(());
                             break;
                         },
                         Some(DataChannelEvent::OnError) => {
-                            let _ = done_tx.try_send(());
                             println!("{}", "datachannel error".to_string().red().bold());
+                            let _ = done_tx.try_send(());
                             break;
                         },
+                        Some(_) => {
+                            println!("{}", "datachannel other event".to_string().bold());
+                        },
                         None => {
-                            //let _ = done_tx.try_send(());
-                            println!("none event");
+                            println!("{}", "datachannel none event".to_string().magenta().bold());
+                            let _ = done_tx.try_send(());
                             break;
-                        }
-                        _ => {
-                            println!("other datachannel event");
                         }
                     }
                 }
@@ -149,26 +165,36 @@ impl PeerConnectionEventHandler for OfferHandler {
     }
 
     async fn on_connection_state_change(&self, state: RTCPeerConnectionState) {
-        //println!("peer connection state: {state}");
-        if state == RTCPeerConnectionState::Failed {
-            println!("{}", "peer connection failed".to_string().red().bold());
-            let _ = self.done_tx.try_send(());
-        } else if state == RTCPeerConnectionState::Disconnected {
-            println!("{}", "peer connection disconnected".to_string().bright_red().bold());
-            let _ = self.done_tx.try_send(());
-        } else if state == RTCPeerConnectionState::Connected {
-            println!("{}", "peer connection connected".to_string().green().bold());
-        } else if state == RTCPeerConnectionState::Connecting {
-            println!("{}", "peer connection connecting...".to_string().bright_green().bold());
-        } else if state == RTCPeerConnectionState::New {
-            println!("{}", "peer connection new".to_string().cyan().bold());
-        } else if state == RTCPeerConnectionState::Closed {
-            println!("{}", "peer connection closed".to_string().red().bold());
-            let _ = self.done_tx.try_send(());
-        } else {
-            println!("peer connection: {state}");
+        let state_info: String;
+        match state {
+            RTCPeerConnectionState::Failed => {
+                state_info = format!("{}{}", "[PEER CONNECTION]: ".to_string().bold().red(), state.to_string().bold().red());
+                let _ = self.done_tx.try_send(());
+            },
+            RTCPeerConnectionState::Disconnected => {
+                state_info = format!("{}{}", "[PEER CONNECTION]: ".to_string().bold().yellow(), state.to_string().bold().yellow());
+                let _ = self.done_tx.try_send(());
+            },
+            RTCPeerConnectionState::Connected => {
+                state_info = format!("{}{}", "[PEER CONNECTION]: ".to_string().bold().green(), state.to_string().bold().green());
+            },
+            RTCPeerConnectionState::Closed => {
+                state_info = format!("{}{}", "[PEER CONNECTION]: ".to_string().bold().yellow(), state.to_string().bold().yellow());
+                let _ = self.done_tx.try_send(());
+            },
+            RTCPeerConnectionState::New => {
+                state_info = format!("{}{}", "[PEER CONNECTION]: ".to_string().bold().bright_blue(), state.to_string().bold().bright_blue());
+            },
+            RTCPeerConnectionState::Connecting => {
+                state_info = format!("{}{}", "[PEER CONNECTION]: ".to_string().bold().bright_green(), state.to_string().bold().bright_green());
+            },
+            RTCPeerConnectionState::Unspecified => {
+                state_info = format!("{}{}", "[PEER CONNECTION]: ".to_string().bold().bright_yellow(), state.to_string().bold().bright_yellow());
+            },
         }
+        println!("{}", &state_info);
     }
+
 }
 
 #[derive(Clone)]
@@ -188,32 +214,42 @@ impl PeerConnectionEventHandler for AnswerHandler {
     }
 
     async fn on_connection_state_change(&self, state: RTCPeerConnectionState) {
-        //println!("Peer connection state: {state}");
-        if state == RTCPeerConnectionState::Failed {
-            println!("{}", "peer connection failed".to_string().red().bold());
-            let _ = self.done_tx.try_send(());
-        } else if state == RTCPeerConnectionState::Disconnected {
-            println!("{}", "peer connection disconnected".to_string().bright_red().bold());
-            let _ = self.done_tx.try_send(());
-        } else if state == RTCPeerConnectionState::Connected {
-            println!("{}", "peer connection connected".to_string().green().bold());
-        } else if state == RTCPeerConnectionState::Connecting {
-            println!("{}", "peer connection connecting...".to_string().bright_green().bold());
-        } else if state == RTCPeerConnectionState::New {
-            println!("{}", "peer connection new".to_string().cyan().bold());
-        } else if state == RTCPeerConnectionState::Closed {
-            println!("{}", "peer connection closed".to_string().red().bold());
-            let _ = self.done_tx.try_send(());
-        } else {
-            println!("peer connection: {state}");
+        let state_info: String;
+        match state {
+            RTCPeerConnectionState::Failed => {
+                state_info = format!("{}{}", "[PEER CONNECTION]: ".to_string().bold().red(), state.to_string().bold().red());
+                let _ = self.done_tx.try_send(());
+            },
+            RTCPeerConnectionState::Disconnected => {
+                state_info = format!("{}{}", "[PEER CONNECTION]: ".to_string().bold().yellow(), state.to_string().bold().yellow());
+                let _ = self.done_tx.try_send(());
+            },
+            RTCPeerConnectionState::Connected => {
+                state_info = format!("{}{}", "[PEER CONNECTION]: ".to_string().bold().green(), state.to_string().bold().green());
+            },
+            RTCPeerConnectionState::Closed => {
+                state_info = format!("{}{}", "[PEER CONNECTION]: ".to_string().bold().yellow(), state.to_string().bold().yellow());
+                let _ = self.done_tx.try_send(());
+            },
+            RTCPeerConnectionState::New => {
+                state_info = format!("{}{}", "[PEER CONNECTION]: ".to_string().bold().bright_blue(), state.to_string().bold().bright_blue());
+            },
+            RTCPeerConnectionState::Connecting => {
+                state_info = format!("{}{}", "[PEER CONNECTION]: ".to_string().bold().bright_green(), state.to_string().bold().bright_green());
+            },
+            RTCPeerConnectionState::Unspecified => {
+                state_info = format!("{}{}", "[PEER CONNECTION]: ".to_string().bold().bright_yellow(), state.to_string().bold().bright_yellow());
+            },
         }
+        println!("{}", &state_info);
     }
+
 
     async fn on_data_channel(&self, dc: Arc<dyn DataChannel>) {
         let done_tx = self.done_tx.clone();
         self.runtime.spawn(Box::pin(async move {
             let mut opened = false;
-            let mut send_timer = Box::pin(sleep(Duration::from_secs(5)));
+            //let mut send_timer = Box::pin(sleep(Duration::from_secs(5)));
             loop {
                 if opened {
                     futures::select! {
@@ -231,43 +267,42 @@ impl PeerConnectionEventHandler for AnswerHandler {
                                 _ => {}
                             }
                         }
-                        _ = send_timer.as_mut().fuse() => {
-                            let message = generate_description(32);
-                            println!("<== '{message}'");
-                            let _ = dc.send(BytesMut::from(message.as_bytes())).await;
-                            send_timer = Box::pin(sleep(Duration::from_secs(5)));
-                        }
+                        //_ = send_timer.as_mut().fuse() => {
+                            //let message = generate_description(32);
+                            //println!("<== '{message}'");
+                            //let _ = dc.send(BytesMut::from(message.as_bytes())).await;
+                            //send_timer = Box::pin(sleep(Duration::from_secs(5)));
+                        //}
                     }
                 } else {
                     match dc.poll().await {
                         Some(DataChannelEvent::OnOpen) => {
                             println!("{}", "datachannel open".to_string().green().bold());
                             opened = true;
-                            send_timer = Box::pin(sleep(Duration::from_secs(5)));
+                            //send_timer = Box::pin(sleep(Duration::from_secs(5)));
                         }
                         Some(DataChannelEvent::OnClose) => {
-                            let _ = done_tx.try_send(());
-                            //println!("datachannel close");
                             println!("{}", "datachannel closed".to_string().red().bold());
+                            let _ = done_tx.try_send(());
                             break;
                         },
                         Some(DataChannelEvent::OnClosing) => {
-                            let _ = done_tx.try_send(());
                             println!("{}", "datachannel closing".to_string().yellow().bold());
+                            let _ = done_tx.try_send(());
                             break;
                         },
                         Some(DataChannelEvent::OnError) => {
-                            let _ = done_tx.try_send(());
                             println!("{}", "datachannel error".to_string().red().bold());
+                            let _ = done_tx.try_send(());
                             break;
                         },
+                        Some(_) => {
+                            println!("{}", "datachannel other event".to_string().bold());
+                        },
                         None => {
+                            println!("{}", "datachannel none event".to_string().magenta().bold());
                             let _ = done_tx.try_send(());
-                            println!("none event");
                             break;
-                        }
-                        _ => {
-                            println!("other datachannel event");
                         }
                     }
                 }
